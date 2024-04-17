dc69c1dd2b57a324754d9ff043f1a4b213789e20
mark0822 <mark20020822@gmail.com>
2024-04-17 08:56:30 -0700

    Initial Commit

---
+++ lifecounter/lifecounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterApp.swift
+//  lifecounter
+//
+//  Created by Mark on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct lifecounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
